46c45be7973ce7d9383e43e3d69906e70e17d728
unknown <mayurpawar8691@gmail.com>
2022-04-05 14:42:16 +0530

    file is added

---
+++ Main.java
@@ -0,0 +1,52 @@
+package com.bl.basic;
+
+import java.util.Date;
+
+public class Main {
+	// Primitive Datatypes
+	int a = 10;
+	byte b = 20;
+	short c = 22;
+	long d = 23;
+	float f = 24.0f
+	double f = 24.33d;
+	boolean g = true;
+	char h = 'a';
+	
+	//class data types which are also called as wrapper data types
+	Integer a1 = 30;
+	Byte b1 = 33;
+	Short c1 = 34;
+	Long d1 = 3551;
+	Float e1 = 344.44f;
+	Double f1 = 55.55d;
+	Boolean g1 = true;
+	Character h1 = 'b';
+	
+	// class data types
+	String i = "hello";
+	Main m = new Main();
+	Stringstr = new String();
+	Date dt = new Date();
+	A aObj = new A();
+	
+	public static void main(String[] args) {
+		System.out.println("Welcome to java basics");
+		
+		A a = new A();
+		a.add(1, 20);
+		a.add(1, "HI");
+	}
+}
+
+class A {	
+
+	
+	
+	
+	
+	
+	
+	
+	// class data types
+	
